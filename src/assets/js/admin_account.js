dbcf1d2b1fbcb8d436b49d8ec520f495a67eb4f2
choco <eudora001010@gmail.com>
2024-05-25 22:49:52 +0800

    first commit

---
+++ src/assets/js/admin_account.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import Bheader from "@/components/Bheader.vue";
+
+// import TaskContainer from '@/components/TaskContainer.vue';
+
+createApp(Bheader).mount("#header");
+// 掛載到app上
+// createApp(TaskContainer).mount("#app");
